a82a45fc34b3a30e2d58358c3bd022c8a7334b1b
Mitchell139 <50130291+Mitchell139@users.noreply.github.com>
2026-02-25 13:11:19 +1000

    Fix database password error

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config'; // Not sure why this is required
 
 @Injectable()
 export class DatabaseService extends PrismaClient {
